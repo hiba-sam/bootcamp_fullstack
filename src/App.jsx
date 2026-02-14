bb1696e3369bc9f04ed22b78114695d1e545f839
hiba-sam <222238502606@etu.univ-usto.dz>
2026-02-14 22:18:29 +0100

    Refactor voting functionality and update anecdotes

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,39 @@ const App = () => {
     'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
     'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'Debugging is twice as hard as writing the code in the first place.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays.',
     'The only way to go fast, is to go well.'
   ]
-   
+
   const [selected, setSelected] = useState(0)
 
-  const handleClick = () => {
+  // create array filled with zeros (same length as anecdotes)
+  const [votes, setVotes] = useState(
+    Array(anecdotes.length).fill(0)
+  )
+
+  const handleNext = () => {
     const randomIndex = Math.floor(Math.random() * anecdotes.length)
     setSelected(randomIndex)
+  }
+
+  const handleVote = () => {
+    const copy = [...votes]   // make a copy
+    copy[selected] += 1       // increment vote
+    setVotes(copy)            // update state
   }
 
   return (
     <div>
       <p>{anecdotes[selected]}</p>
-      <button onClick={handleClick}>
+      <p>has {votes[selected]} votes</p>
+
+      <button onClick={handleVote}>
+        vote
+      </button>
+
+      <button onClick={handleNext}>
         next anecdote
       </button>
     </div>
