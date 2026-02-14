36d3867da21d5a2e96a2e3c8e7ad704d927e9c87
hiba-sam <222238502606@etu.univ-usto.dz>
2026-02-14 23:28:14 +0100

    Delete src/components directory

--- src/components/course.jsx
+++
@@ -1,7 +0,0 @@
-const Total = ({ parts }) => {
-  const sum = parts.reduce((total, part) => {
-    console.log('Adding exercises:', total, part.exercises)
-    return total + part.exercises
-  }, 0)
-  return <p><strong>Total exercises: {sum}</strong></p>
-}
