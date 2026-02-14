ff8c8f827c6877947e452459096b00f05588afdc
hiba-sam <222238502606@etu.univ-usto.dz>
2026-02-14 23:02:17 +0100

    Update App.jsx
    Added Header, Part, Content, and Total components to structure course information.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,32 @@
-import Course from './components/Course'
+const Header = ({ name }) => <h1>{name}</h1>
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+)
+
+// New Total component
+const Total = ({ parts }) => {
+  const sum = parts.reduce((acc, part) => acc + part.exercises, 0)
+  return <p><strong>Total of exercises: {sum}</strong></p>
+}
+
+const Course = ({ course }) => (
+  <div>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+)
 
 const App = () => {
   const course = {
