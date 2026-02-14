556cdf735ec098fe2aa043b9a372c0b1cf98339e
hiba-sam <222238502606@etu.univ-usto.dz>
2026-02-14 22:38:42 +0100

    Implement Total component in Course
    Added Total component to display total exercises.

--- src/components/course.jsx
+++ src/components/course.jsx
@@ -1,24 +1,30 @@
 const Header = ({ name }) => <h2>{name}</h2>
 
 const Part = ({ part }) => (
-    <p>
-        {part.name} {part.exercises}
-    </p>
+  <p>
+    {part.name} {part.exercises}
+  </p>
 )
 
 const Content = ({ parts }) => (
-    <>
-        {parts.map(part => (
-            <Part key={part.id} part={part} />
-        ))}
-    </>
+  <>
+    {parts.map(part => (
+      <Part key={part.id} part={part} />
+    ))}
+  </>
 )
 
+const Total = ({ parts }) => {
+  const sum = parts.reduce((total, part) => total + part.exercises, 0)
+  return <p><strong>Total exercises: {sum}</strong></p>
+}
+
 const Course = ({ course }) => (
-    <div>
-        <Header name={course.name} />
-        <Content parts={course.parts} />
-    </div>
+  <div>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
 )
 
 export default Course
