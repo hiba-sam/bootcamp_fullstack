53ee74af239761ad0b7eed4410cad964b4a59a9d
hiba-sam <222238502606@etu.univ-usto.dz>
2026-02-14 21:08:19 +0100

    Refactor App component to use individual part objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-const Header = (props) => {
-  return (<div> <h1>{props.course}</h1> </div>)
-}
-const Content = (props) => {
-  return (<div> <p>{props.part} {props.exercises}</p> </div>)
-}
-const Total = (props) => {
-  return (<div> <p>Number of exercises {props.exercises}</p> </div>)
-}
 const App = () => {
   const course = 'Half Stack application development'
-  const parts = [{ part: 'Fundamentals of React', exercises: 10 }, { part: 'Using props to pass data', exercises: 7 }, { part: 'State of a component', exercises: 14 }]
-  return (<div> <Header course={course} /> <Content part={parts[0].part} exercises={parts[0].exercises} /> <Content part={parts[1].part} exercises={parts[1].exercises} /> <Content part={parts[2].part} exercises={parts[2].exercises} /> <Total exercises={parts[0].exercises + parts[1].exercises + parts[2].exercises} /> </div>)
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <h1>{course}</h1>
+      <p>{part1.name} {part1.exercises}</p>
+      <p>{part2.name} {part2.exercises}</p>
+      <p>{part3.name} {part3.exercises}</p>
+      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
+    </div>
+  )
 }
+
 export default App
