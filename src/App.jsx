77d6dcedb5cd9b1da1f64f1dc8560dd620b2b002
hiba-sam <222238502606@etu.univ-usto.dz>
2026-02-14 22:21:45 +0100

    Refactor anecdotes and voting button layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...',
+    'Any fool can write code that a computer can understand.',
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays.',
@@ -13,8 +13,6 @@ const App = () => {
   ]
 
   const [selected, setSelected] = useState(0)
-
-  // create array filled with zeros (same length as anecdotes)
   const [votes, setVotes] = useState(
     Array(anecdotes.length).fill(0)
   )
@@ -25,23 +23,27 @@ const App = () => {
   }
 
   const handleVote = () => {
-    const copy = [...votes]   // make a copy
-    copy[selected] += 1       // increment vote
-    setVotes(copy)            // update state
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
   }
+
+  // find index of anecdote with most votes
+  const maxVotes = Math.max(...votes)
+  const mostVotedIndex = votes.indexOf(maxVotes)
 
   return (
     <div>
+      <h2>Anecdote of the day</h2>
       <p>{anecdotes[selected]}</p>
       <p>has {votes[selected]} votes</p>
 
-      <button onClick={handleVote}>
-        vote
-      </button>
+      <button onClick={handleVote}>vote</button>
+      <button onClick={handleNext}>next anecdote</button>
 
-      <button onClick={handleNext}>
-        next anecdote
-      </button>
+      <h2>Anecdote with most votes</h2>
+      <p>{anecdotes[mostVotedIndex]}</p>
+      <p>has {maxVotes} votes</p>
     </div>
   )
 }
