d296817255181e68b6092aca45afbb066f1d6763
hiba-sam <222238502606@etu.univ-usto.dz>
2026-02-14 19:50:31 +0100

    Refactor App component to use Header, Content, and Total

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.part1.name} exercises={props.part1.exercises} />
+      <Part name={props.part2.name} exercises={props.part2.exercises} />
+      <Part name={props.part3.name} exercises={props.part3.exercises} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}
+    </p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
+
   const part1 = {
     name: 'Fundamentals of React',
     exercises: 10
   }
+
   const part2 = {
     name: 'Using props to pass data',
     exercises: 7
   }
+
   const part3 = {
     name: 'State of a component',
     exercises: 14
@@ -15,11 +48,9 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>{part1.name} {part1.exercises}</p>
-      <p>{part2.name} {part2.exercises}</p>
-      <p>{part3.name} {part3.exercises}</p>
-      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total part1={part1} part2={part2} part3={part3} />
     </div>
   )
 }
