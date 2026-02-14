7bfe74d5c8142ea33ac0b909d30c583c36db111c
hiba-sam <222238502606@etu.univ-usto.dz>
2026-02-14 23:12:41 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,31 @@
-import Course from './components/Course'
+const Header = ({ name }) => <h1>{name}</h1>
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+)
+
+const Total = ({ parts }) => {
+  const sum = parts.reduce((acc, part) => acc + part.exercises, 0)
+  return <p><strong>Total of exercises: {sum}</strong></p>
+}
+
+const Course = ({ course }) => (
+  <div>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+)
 
 const App = () => {
   const courses = [
@@ -11,7 +38,7 @@ const App = () => {
         { name: 'State of a component', exercises: 14, id: 3 },
         { name: 'Redux', exercises: 11, id: 4 }
       ]
-    },
+    }, 
     {
       name: 'Node.js',
       id: 2,
