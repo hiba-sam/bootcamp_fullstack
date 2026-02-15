d6f8cd637af18223365fde81082fc3d20150b43c
hiba-sam <222238502606@etu.univ-usto.dz>
2026-02-15 19:47:03 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [notification, setNotification] = useState(null)
+  const [notificationType, setNotificationType] = useState(null)
+
 
   useEffect(() => {
     personService
@@ -24,76 +27,81 @@ const App = () => {
         const updatedPerson = { ...existingPerson, number: newNumber }
 
         personService
-          .update(existingPerson.id, updatedPerson)
+          .create(personObject)
           .then(returnedPerson => {
-            setPersons(
-              persons.map(p =>
-                p.id === existingPerson.id ? returnedPerson : p
-              )
-            )
+            setPersons(persons.concat(returnedPerson))
+
+            setNotification(`Added ${returnedPerson.name}`)
+            setNotificationType('success')
+
+            setNotification(`Updated ${returnedPerson.name}`)
+            setNotificationType('success')
+
+            setTimeout(() => {
+              setNotification(null)
+            }, 5000)
+
+          })
+
+      } else {
+        const personObject = {
+          name: newName,
+          number: newNumber
+        }
+
+        personService
+          .create(personObject)
+          .then(returnedPerson => {
+            setPersons(persons.concat(returnedPerson))
             setNewName('')
             setNewNumber('')
           })
       }
-    } else {
-      const personObject = {
-        name: newName,
-        number: newNumber
+    }
+
+
+    const deletePerson = (id) => {
+      const person = persons.find(p => p.id === id)
+
+      if (window.confirm(`Delete ${person.name}?`)) {
+        personService
+          .remove(id)
+          .then(() => {
+            setPersons(persons.filter(p => p.id !== id))
+          })
       }
-
-      personService
-        .create(personObject)
-        .then(returnedPerson => {
-          setPersons(persons.concat(returnedPerson))
-          setNewName('')
-          setNewNumber('')
-        })
     }
+
+    return (
+      <div>
+        <h2>Phonebook</h2>
+
+        <form onSubmit={addPerson}>
+          <div>
+            name:
+            <input value={newName}
+              onChange={(e) => setNewName(e.target.value)} />
+          </div>
+          <div>
+            number:
+            <input value={newNumber}
+              onChange={(e) => setNewNumber(e.target.value)} />
+          </div>
+          <button type="submit">add</button>
+        </form>
+
+        <h2>Numbers</h2>
+        {persons.map(person =>
+          <p key={person.id}>
+            {person.name} {person.number}
+            <button onClick={() => deletePerson(person.id)}>
+              delete
+            </button>
+          </p>
+        )}
+        <Notification message={notification} type={notificationType} />
+      </div>
+    )
   }
 
-
-  const deletePerson = (id) => {
-    const person = persons.find(p => p.id === id)
-
-    if (window.confirm(`Delete ${person.name}?`)) {
-      personService
-        .remove(id)
-        .then(() => {
-          setPersons(persons.filter(p => p.id !== id))
-        })
-    }
-  }
-
-  return (
-    <div>
-      <h2>Phonebook</h2>
-
-      <form onSubmit={addPerson}>
-        <div>
-          name:
-          <input value={newName}
-            onChange={(e) => setNewName(e.target.value)} />
-        </div>
-        <div>
-          number:
-          <input value={newNumber}
-            onChange={(e) => setNewNumber(e.target.value)} />
-        </div>
-        <button type="submit">add</button>
-      </form>
-
-      <h2>Numbers</h2>
-      {persons.map(person =>
-        <p key={person.id}>
-          {person.name} {person.number}
-          <button onClick={() => deletePerson(person.id)}>
-            delete
-          </button>
-        </p>
-      )}
-
-    </div>
-  )
-}
-
-export default App
+  export default App
